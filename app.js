6610dade7eda723437079d6924e397db74ca2fa5
Jos <jos@cninone.com>
2018-02-02 14:38:32 +0800

    递归优化并发

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const youka = new (require('./youka'))();
-const pfs   = require('./pfs');
+const youka = new (require('./lib/youka'))();
+const pfs   = require('./lib/pfs');
 
 // var arguments = process.argv.splice(2);
 // var qq;
@@ -13,41 +13,56 @@ const pfs   = require('./pfs');
 
 // if (typeof qq == 'undefined') console.log(new Error('请在命令后面添加-qq参数 例如 node app.js -qq 123456'));
 
-const file = __dirname + '/cardinfo/' + (new Date()).getTime() + (Math.round(Math.random() * 1000)) + '.txt';
-
 pfs.readFile('./qq.txt')
     .then(text => {
-        text.split('\n').map(qq => {
-            youka.getCookiess(qq)
-                .then(cookies => {
-                    return youka.getChkcode(cookies, false);
-                })
-                .then(obj => {
-                    return youka.getList(obj.cookies, obj.chkcode, qq);
-                })
-                .then(obj => {
-                    let $ = obj.$;
-                    $('.order_tb a').each((index, element) => {
-                        const href = element.attribs.href;
-                        const orderid = href.substr(href.indexOf('=') + 1);
-
-                        youka.getData(orderid, obj.cookies)
-                            .then(cardinfo => {
-                                pfs.writeFile(file, JSON.stringify(cardinfo) + '\t\n\n')
-                                    .then(file => {
-                                        console.log(`订单${orderid}内卡密写入文件${file}成功`);
-                                    }, err => {
-                                        console.log(err);
-                                    })
-                            }, err => {
-                                console.log(`订单${orderid}查询失败，失败原因：${err}`);
-                            });
-                    });
-                })
-                .catch(err => {
-                    //验证码输入错误
-                    console.log(`${qq}获取列表失败，失败原因：${err}`);
-                });
-            return qq;
-        });
+        let qqs = text.split('\n');
+        getData(qqs);
     });
+
+function getData (qqs1, qqs2) {
+    qqs2 = qqs2 || [];
+
+    const diffqqs = qqs1.filter(key => !qqs2.includes(key));
+
+    if (diffqqs.length == 0) return false;
+    
+    const qq = diffqqs[0];
+
+    const file = __dirname + '/cardinfo/' + (new Date()).getTime() + (Math.round(Math.random() * 1000)) + '.txt';
+
+    youka.getCookiess(qq)
+    .then(cookies => {
+        return youka.getChkcode(cookies, false);
+    })
+    .then(obj => {
+        return youka.getList(obj.cookies, obj.chkcode, qq);
+    })
+    .then(obj => {
+        qqs2.push(qq);
+        let $ = obj.$;
+        if ($('.order_tb a').length <= 0) console.log(`qq${qq}内没有订单\t\n`);
+        $('.order_tb a').each((index, element) => {
+            const href = element.attribs.href;
+            const orderid = href.substr(href.indexOf('=') + 1);
+
+            youka.getData(orderid, obj.cookies)
+                .then(cardinfo => {
+                    pfs.writeFile(file, JSON.stringify(cardinfo) + '\t\n\n')
+                        .then(file => {
+                            console.log(`订单${orderid}内卡密写入文件${file}成功\t\n`);
+                        }, err => {
+                            console.log('文件写入失败', err, '\t\n');
+                        })
+                }, err => {
+                    console.log(`订单${orderid}查询失败，失败原因：${err}\t\n`);
+                });
+        });
+
+        getData(qqs1, qqs2);
+    })
+    .catch(err => {
+        getData(qqs1, qqs2);
+        //验证码输入错误
+        console.log(`qq${qq}获取列表失败，失败原因：${err}\t\n`);
+    });
+}
